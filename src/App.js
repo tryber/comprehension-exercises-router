52d272b66fc06d23800522235ca830254176ef74
Giovanna Betti <giovannabetti@gmail.com>
2021-01-24 23:49:08 -0300

    exercise for fixation about router content

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 import Home from './Home';
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
+import About from './About';
+import Users from './Users';
+import StrictAccess from './StrictAccess';
 
 class App extends Component {
   render() {
     return (
-      <Home />
+      <BrowserRouter>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/users">Users</Link>
+        <Link to="/strict-access">Strict Access</Link>
+      </BrowserRouter>
     );
   }
 }
