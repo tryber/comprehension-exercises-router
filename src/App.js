d87ea2d40c49bdd19d5327bf439e2f83c957413f
rodolfo_oliveira <rod.vgo@gmail.com>
2021-02-02 02:12:13 -0300

    requisitos 1 e 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 import Home from './components/Home';
+import { BrowserRouter, Route } from 'react-router-dom'
+import About from './components/About';
+import Users from './components/Users';
+
 
 class App extends Component {
   render() {
     return (
-      <Home />
+      <BrowserRouter>
+        <Route path='/about' component={About} />
+        <Route path='/users' component={Users} />
+        <Route exact path='/' component={Home} />
+      </BrowserRouter>
     );
   }
 }
--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class About extends Component {
   render() {
@@ -6,6 +7,8 @@ class About extends Component {
       <div>
         <h2>About</h2>
         <p> My awesome About component </p>
+
+        <Link to='/'>Voltar a Home</Link>
       </div>
     );
   }
