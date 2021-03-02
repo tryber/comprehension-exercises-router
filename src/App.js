aefdc688b218e50338b99f1a362a1ad903c75fbd
LucioOSilva <lucioosilva00@gmail.com>
2021-03-02 17:26:57 -0300

    tasks up 1 to 6

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 import Home from './components/Home';
+import About from './components/About';
+import Users from './components/Users';
 
 class App extends Component {
   render() {
     return (
-      <Home />
+      <BrowserRouter>
+        <Link to="/">Home</Link>
+        <Link to="/about">Users</Link>
+        <Link to="/users">About</Link>
+
+        <Switch>
+          <Route path="/about"><About /></Route>
+          <Route path="/users" component={Users} />
+          <Route path="/" component={Home} />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
