12f43f1d0575a5b5e1c02bee1d110ca8b4aa4cfa
Aladino Borges <eualadino@gmail.com>
2021-05-06 17:07:48 -0300

    Requisito 3.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import Home from './components/Home';
-import { BrowserRouter, Link } from 'react-router-dom';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Home />
+        <Route exact path="/">
+          <Home />
+        </Route>
+        
         <ul>
           <Link to="/"> Home </Link>
           <Link to="/about"> About </Link>
