529aa5930c8f25d196b62a348316971856b3aa0c
lucasportella <lucasportellaagu@gmail.com>
2021-05-06 10:02:02 -0300

    req 2-6

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import React, { Component } from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import Home from './components/Home';
+import React, { Component } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Users from "./components/Users";
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-            <Home />
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Ir para Home</Link>
+            </li>
+            <li>
+              <Link to="/About">Ir para About</Link>
+            </li>
+            <li>
+              <Link to="/Users">Ir para Users</Link>
+            </li>
+          </ul>
+        </nav>
+        <Route exact path="/" component={Home}></Route>
+        <Route exact path="/About" component={About}></Route>
+        <Route exact path="/Users" component={Users}></Route>
       </BrowserRouter>
     );
   }
