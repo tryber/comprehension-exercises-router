361d259e3916832c997dbdae886201beb71cf758
Victor Canto <iamvictorcanto@gmail.com>
2021-05-10 16:42:45 -0300

    comprehension-exercises-router complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
 import Home from './components/Home';
+import About from './components/About';
+import Users from './components/Users';
+import {BrowserRouter as Router, Link, Route, Switch} from 'react-router-dom'
+import StrictAccess from './StrictAccess';
 
 class App extends Component {
   render() {
     return (
-      <Home />
+      <Router>
+        <Switch>
+          <Route exact path="/users/:id" render={(props) => <Users {...props} greetingMessage={"Good Morning"} />} />
+          <Route path="/about" component={About} />
+          <Route exact path="/" component={Home} />
+          <Route path="/strict-access" render={(props) => <StrictAccess {...props} user={{username: 'kawaki', password: 'ninja'}} />} />
+        </Switch>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/users">Users</Link>
+        <Link to="/strict-access">Strict Access</Link>
+      </Router>
     );
   }
 }
--- src/components/Users.js
+++ src/components/Users.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react';
 
 class Users extends Component {
   render() {
+    const {greetingMessage} = this.props;
+    const {match: {params: {id}}} = this.props;
     return (
       <div>
         <h2>Users</h2>
-        <p> My awesome Users component </p>
+        <p> {greetingMessage} user n° {id}, this is my awesome Users component </p>
       </div>
     );
   }
