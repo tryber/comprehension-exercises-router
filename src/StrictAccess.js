f6f2737058bd1812ff5a648c1efc11b96c104bbc
andy silva <andssilva.23@gmail.com>
2021-05-13 10:28:31 -0400

    feat: :rocket: requirement 11

---
+++ src/StrictAccess.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Redirect } from 'react-router';
+
+class StrictAccess extends Component {
+  render() {
+    const { username, password } = this.props.user;
+    
+    if (username !== 'joao' && password !== 1234) {
+      alert('Access denied');
+      return <Redirect to="/" />
+    }
+
+    return (
+      <div>
+        <p>Welcome João!! =D</p>
+      </div>
+    );
+  }
+}
+
+export default StrictAccess;
