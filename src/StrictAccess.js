6041504224a45d8a49e6b252493482e81aeecf0c
andy silva <andssilva.23@gmail.com>
2021-05-13 10:29:50 -0400

    fix: :bug: requirement 11

--- src/StrictAccess.js
+++ src/StrictAccess.js
@@ -4,8 +4,8 @@ import { Redirect } from 'react-router';
 class StrictAccess extends Component {
   render() {
     const { username, password } = this.props.user;
-    
-    if (username !== 'joao' && password !== 1234) {
+
+    if (username !== 'joao' || password !== 1234) {
       alert('Access denied');
       return <Redirect to="/" />
     }
