e98cd8c585e4cc0a2e3469b9c9dc1ccadf2b5f84
Hamander <1hamander@gmail.com>
2021-06-07 21:04:42 -0300

    Exercicios de Fixacao Finalizado

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react';
+import { Link, BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import Users from './components/Users';
-import { Route } from 'react-router-dom';
-import { BrowserRouter } from 'react-router-dom';
+import StrictAcess from './components/StrictAcess';
 
 class App extends Component {
+
   render() {
+    const user = {
+      username: 'joao',
+      password: '1234',
+    }
+    
     return (
       <BrowserRouter >
-        <Route exact path="/" component={Home}/>
-        <Route path="/about" component={About}/>
-        <Route path="/users" component={Users}/>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/users">Users</Link>
+      <Link to="/strictAcess">FBI</Link>
+        <Switch>
+          <Route path="/strictAcess" render={ () => (<StrictAcess user={ user }/>)}></Route>
+          <Route exact path="/" component={Home}/>
+          <Route path="/about" component={About}/>
+          <Route path="/users/:id" render={ (props) => <Users { ...props } greetingsMessage="Good Morning"/> }/>
+        </Switch>
       </BrowserRouter>
     );
   }
