cbb5c05a24fe6bf911862d7db6c4ace440e9e576
Júlio Mota <juliomotacampos@gmail.com>
2021-06-11 16:00:23 -0300

    Adiciona exercicio 3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom'
 import Home from './components/Home';
+import About from './components/About';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <Route path="/" component={ Home } />
+        <Route path="/about" component={ About } />
       </BrowserRouter>
     );
   }
