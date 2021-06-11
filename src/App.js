7166083a01a36e19239e3bfaf8781f30bc8c73bf
Júlio Mota <juliomotacampos@gmail.com>
2021-06-11 16:11:23 -0300

    Adiciona exercicio 6

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Link } from 'react-router-dom'
 import Home from './components/Home';
 import About from './components/About';
 import Users from './components/Users';
 
 class App extends Component {
   render() {
+    const links = [
+      {link: '/', component: 'Home'},
+      {link: '/about', component: 'About'},
+      {link: '/users', component: 'Users'}
+    ];
+
     return (
       <BrowserRouter>
         <Route exact path="/" component={ Home } />
         <Route path="/about" component={ About } />
         <Route path="/users" component={ Users } />
+
+        { links.map(({ link, component }) => (
+          <Link to={ link } key={ component }>{ component }</Link>
+        ))}
       </BrowserRouter>
     );
   }
