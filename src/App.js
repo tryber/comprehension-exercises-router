46c08f4cafadb814cc3d2e921999f531f19e44d6
matheus oliveira de proenca <mproenca.marketing@gmail.com>
2021-07-05 18:41:00 -0300

    adicionando as rotas dos  Links e dando padding para os Links

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react';
+import './App.css'
 import Home from './components/Home';
 import About from './components/About'
 import Users from './components/Users'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Link } from 'react-router-dom'
 
 class App extends Component {
   render() {
+    const links = [
+      {link: '/', component: 'Home'},
+      {link: '/About', component: 'About'},
+      {link: '/Users', component: 'Users'},
+    ]
+
     return (
       <BrowserRouter>
         <Route exact path="/" component={ Home } />
         <Route path="/About" component={ About } />
         <Route path="/Users" component={ Users } />
+        { links.map(({ link, component })=> (
+          <Link className="links" to={ link } key={ component }>{ component }
+          </Link>
+        ))}
       </BrowserRouter>
     );
   }
