6e3fc4d812d5108a2fae236e4eb2ae19824221d4
Lucas Fernandes <lucas.h.fernandes.reis@gmail.com>
2021-07-05 16:08:39 -0300

    Realiza uso do componente Link na aplicaçao

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import Home from './components/Home';
-import { BrowserRouter } from 'react-router-dom';
+import About from './components/About';
+import Users from './components/Users';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Home />
+        <Link to='/'>Home</Link>
+        <Link to='/about'>About</Link>
+        <Link to='/users'>Users</Link>
+        <Route exact path='/' component={ Home } />
+        <Route path='/about' component={ About } />
+        <Route path='/users' render={(props) => <Users {...props} greetingsMessage="Good Morning" />} />
       </BrowserRouter>
     );
   }
