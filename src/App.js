6b501dfe67eeedc118c304a84659821a1aca1ee2
Daniel <danielbped@gmail.com>
2021-07-05 15:26:27 -0300

    Resolve requisitos 1 e 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import Home from './components/Home';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <Home />
+      <BrowserRouter>
+        <Route exact path='/' component={ Home } />
+      </BrowserRouter>
     );
   }
 }
