966dd8d22ac5a25b621cec599125c1c87a1e7b6f
José Carlos <kalarruda@hotmail.com>
2021-07-05 13:16:11 -0300

    adiciona links

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import About from './components/About';
 import Home from './components/Home';
+import Users from './components/Users';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Home />
+        <nav>
+          <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/about">About</Link></li>
+              <li><Link to="/users">Users</Link></li>
+            </ul>
+        </nav>
+        <Route exact path="/users" component={ Users } />
+        <Route exact path="/about" component={ About } />
+        <Route exact path="/" component={ Home } />
       </BrowserRouter>      
     );
   }
